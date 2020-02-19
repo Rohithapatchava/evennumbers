0c40fcc33d4eff783ae5a85acc3f489e1b9d3f29
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-19 17:40:53 +0530

    Initial commit

---
+++ evennumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evennumbers
+{
+    class Program
+    {
+        public static void even(int[] arr)
+        {
+            int sum = 0;
+            foreach (int i in arr)
+            {
+                if (i % 2 == 0)
+                {
+                    sum = sum + i;
+
+                }
+            }
+                Console.WriteLine(sum);
+            
+        }
+        static void Main(string[] args)
+        {
+           
+            int[] value= new int[100];
+            Console.WriteLine("enter the numbers");
+            for (int i = 0; i < 5; i++)
+            {
+                value[i] = int.Parse(Console.ReadLine());
+
+            }
+            even(value);
+        }
+    }
+}
